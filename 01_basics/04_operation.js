860427160cac25ed2d735088d115aab554482c7b
Gulshan Kumar <gulshangulatimtsn181@gmail.com>
2024-08-02 07:59:03 +0000

    Operation

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,51 @@
+// // // // let value = 3
+// // // // let negValue = -value
+// // // // console.log(negValue)
+
+// // // // console.log(2+2);
+// // // // console.log(2-2);
+// // // // console.log(2*2);     //mul
+// // // // console.log(2**3);    //2 ^3
+// // // // console.log(2/3);     //devide
+// // // // console.log(2%3);    //modulus
+
+// // // let str1 = "Gullu"
+// // // let str2 = "Hello"
+
+// // // let str3 = str1 + str2;
+// // // console.log(str3);
+
+// // // console.log("1" + 2);
+// // // console.log(1 + "2");
+// // // console.log("1" + 2 + 2);
+// // // console.log(1 + 2 + "2")
+// // // console.log(3+4*5 % 3)
+
+// // console.log(+true);
+// // console.log(+"");
+
+// let gameCounter = 100;
+// ++gameCounter;
+// console.log(gameCounter);
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+// console.log("2" > 1);
+// console.log("02">1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+
+console.log("2" === 2);
+console.log("2" == 2);
+
+
