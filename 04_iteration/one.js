241597b546b179789556ea742466990af01515b8
Gulshan Kumar <gulshangulatimtsn181@gmail.com>
2024-08-14 19:52:40 +0000

    forEachloop

--- 04_iteration/one.js
+++ 04_iteration/one.js
@@ -1,27 +1,52 @@
-// for(let index = 0 ; index < 10; index++){
-//     console.log(index);
-// }
+// // for(let index = 0 ; index < 10; index++){
+// //     console.log(index);
+// // }
 
-// for(let i = 0; i<=10; i++){
-//     const element = i;
-//     if(element == 5){
-//         console.log("5 is best number");
-//     }
-//     console.log(element);
+// // for(let i = 0; i<=10; i++){
+// //     const element = i;
+// //     if(element == 5){
+// //         console.log("5 is best number");
+// //     }
+// //     console.log(element);
         
+// // }
+
+// // for(let i = 0; i<=10; i++){
+// //     console.log(`Outer loop value: ${i}`);
+// //     for(let j = 0; j<=10; j++){
+// //         console.log(i + '*' + j + ' = ' + i*j);
+// //     }    
+// // } 
+// // for(let i = 0; i<20; i++){
+// //     if(i == 5){
+// //         console.log(`Detected 5`);
+// //         // break;
+// //         continue
+// //     }
+// //     console.log(`value of i is ${i}`);
+// // }
+
+// // let index = 0;
+// // while(index <= 10){
+// //     console.log(`value of index is ${index}`);
+// //     index = index + 2;
+// // }
+// let myArray = [`Flash`,"batman","superma"]
+// let arr = 0;
+// while(arr < myArray.length){
+//     console.log(`value is ${myArray[arr]}`);
+//     arr = arr + 1
 // }
 
-// for(let i = 0; i<=10; i++){
-//     console.log(`Outer loop value: ${i}`);
-//     for(let j = 0; j<=10; j++){
-//         console.log(i + '*' + j + ' = ' + i*j);
-//     }    
-// } 
-for(let i = 0; i<20; i++){
-    if(i == 5){
-        console.log(`Detected 5`);
-        // break;
-        continue
-    }
-    console.log(`value of i is ${i}`);
-}
+// let score = 11
+// do {
+//     console.log(`Score is ${score}`);
+//     score++
+// } while(score <= 10);
+
+const coding = ["js","rubby","java","python","cpp"]
+const values = coding.forEach((items) =>{
+    console.log(item);
+    return item;
+})
+// console.log(values);
